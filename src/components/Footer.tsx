2f22dc3b6f51ac9576eba85267a866c29df2b23d
Linux Hari <vadiveluversionz1.0@gmail.com>
2024-05-06 22:43:01 +0530

    "change domain name"

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-red-500 py-10">
       <div className="container mx-auto flex flex-col md:flex-row justify-between gap-y-4 items-center">
         <div className="space-y-2 text-white">
-          <h2 className="text-3xl font-bold tracking-tight">MernGrub.com</h2>
+          <h2 className="text-3xl font-bold tracking-tight">MernGrub</h2>
           <p className="text-sm">9097 Olga Corner, Port Johnnie, LA 89912-9230</p>
         </div>
         <span className="text-white font-bold tracking-tight flex gap-4">
